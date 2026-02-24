ff63aaaeee05bbd7c7598588bcc8d28c7240a239
Sofia Nicoletti <sofia.nicoletti@algorand.foundation>
2026-02-24 14:36:28 -0300

    chore(runner): init module with basic lint and format

---
+++ runner/eslint.config.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  prettierConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["*.js"],
+        },
+      },
+    },
+  },
+);
