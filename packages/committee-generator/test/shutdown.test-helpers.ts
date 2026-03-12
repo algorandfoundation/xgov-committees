d19bb384cb1bdf40aee77ca123b16dd7c7cadeaf
Blair McIlroy <blair.mcilroy@algorand.foundation>
2026-03-12 20:19:10 +0700

    refactor: consolidate test helpers in shutdown module and update integration tests

---
+++ packages/committee-generator/test/shutdown.test-helpers.ts
@@ -0,0 +1,6 @@
+/**
+ * Test helper functions for shutdown.ts
+ * These functions are for testing purposes only and provide internal access to shutdown state.
+ */
+
+export { __testInternals } from '../src/shutdown';
