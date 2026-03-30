d4848417d57fc3ece575ae98e4190857db6875b2
Blair McIlroy <blair.mcilroy@algorand.foundation>
2026-03-30 11:22:02 +0700

    feat: add ARC28 event decoding utilities and hash prefix function

--- packages/committee-generator/src/utils.ts
+++ packages/committee-generator/src/utils.ts
@@ -6,6 +6,7 @@ import { createWriteStream } from 'fs';
 import { pipeline } from 'stream/promises';
 import { Readable } from 'stream';
 import { BinaryLike, createHash } from 'crypto';
+import { ABIType } from 'algosdk';
 
 export async function fsExists(path: string) {
   try {
@@ -108,3 +109,66 @@ export async function downloadToFile(url: string, filename: string): Promise<voi
 export function getMD5Hash(buffer: BinaryLike): string {
   return createHash('md5').update(buffer).digest('hex');
 }
+
+export const getARC28Prefix = (eventSignature: string): Buffer<ArrayBufferLike> => {
+  // create sha512-256 hash of event signature
+  const hash = createHash('sha512-256').update(eventSignature).digest();
+  // first 4 bytes are the hash
+  return hash.subarray(0, 4);
+};
+
+/**
+ * Utility to decode event from raw log
+ * @param abiString ABI string of the event
+ * @param rawLog Raw log bytes
+ * @param keys Keys of the event fields
+ * @returns Decoded event T object
+ * @throws Error if event signature does not match expected
+ * @example
+ * ```ts
+ * const event = getARC28EventFromLog<{ previousManager: string; newManager: string }>(
+ *   'ManagerUpdated(address,address)',
+ *   rawLog,
+ *   ['previousManager', 'newManager'],
+ * )
+ * ```
+ * @link https://dev.algorand.co/arc-standards/arc-0028/
+ */
+export const getARC28EventFromLog = <T extends Record<string, any>>(
+  eventSignature: string,
+  rawLog: Uint8Array,
+  keys: (keyof T)[],
+): T => {
+  // create sha512-256 hash prefix (first 4 bytes) of event signature
+  const hash = getARC28Prefix(eventSignature);
+
+  // check first 4 bytes of rawLog match the hash prefix
+  const actualSignature = rawLog.subarray(0, 4);
+
+  // verify signatures match
+  if (hash.compare(actualSignature) !== 0) {
+    throw new Error('Event signature does not match expected signature!');
+  }
+
+  // first 4 bytes are the event signature hash
+  const rawEventData = rawLog.subarray(4);
+  // get ABI data start index (after event name)
+  const abiDataStart = eventSignature.indexOf('(');
+  // decode raw event
+  const decoded = ABIType.from(eventSignature.substring(abiDataStart)).decode(
+    rawEventData,
+  ) as any[];
+  // Map tuple values to named object
+  const result = {} as T;
+
+  if (keys.length !== decoded.length) {
+    throw new Error(
+      `Event field count mismatch: expected ${keys.length} values, but decoded ${decoded.length}.`,
+    );
+  }
+  keys.forEach((key, index) => {
+    result[key] = decoded[index];
+  });
+
+  return result;
+};
